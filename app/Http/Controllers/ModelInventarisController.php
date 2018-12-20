00675a2bf99c762af59cb13888d1918b5736b90c
Cahaya Camila <cahayacamila96@gmail.com>
2018-12-21 02:38:22 +0700

    kokokok

--- app/Http/Controllers/ModelInventarisController.php
+++ app/Http/Controllers/ModelInventarisController.php
@@ -8,7 +8,8 @@ use App\model_inventaris;
 class ModelInventarisController extends Controller
 {
     public function index(){
-    	return model_inventaris::all();
+        $model=model_inventaris::all()
+    	return response()->json(['data'=>$model]);
     } 
 
     public function create (request $request){
