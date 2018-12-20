15f0ef59fbd4b4f5a8854bfcf93e804ca9d6f06d
Cahaya Camila <cahayacamila96@gmail.com>
2018-12-21 05:06:34 +0700

    kokok

--- app/Http/Controllers/ModelInventarisController.php
+++ app/Http/Controllers/ModelInventarisController.php
@@ -9,7 +9,7 @@ class ModelInventarisController extends Controller
 {
     public function index(){
         $model=model_inventaris::all();
-    	return response()->json(['data'=>$model]);
+    	return response()->json($model);
     } 
 
     public function create (request $request){
